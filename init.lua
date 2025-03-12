ddcd52adea26b1d42e7d2a506d6804ee9460ad9b
L. Alberto Giménez <agimenez@sysvalve.es>
2025-03-12 16:50:16 +0100

    Fix go imports weirdness on save

--- init.lua
+++ init.lua
@@ -989,14 +989,14 @@ require('lazy').setup({
   },
 })
 
-local format_sync_grp = vim.api.nvim_create_augroup('goimports', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
+-- local format_sync_grp = vim.api.nvim_create_augroup('go_imports', { clear = true })
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = '*.go',
+--   callback = function()
+--     require('go.format').goimports()
+--   end,
+--   group = format_sync_grp,
+-- })
 
 vim.keymap.set('n', '-', '<Cmd>Oil<CR>')
 -- The line beneath this is called `modeline`. See `:help modeline`
